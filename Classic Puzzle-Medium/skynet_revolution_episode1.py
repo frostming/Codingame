9666751de08fc043ca5c232da40ce561f21975c9
frostming <mianghong@gmail.com>
2017-10-25 09:42:54 +0800

    Skynet revolution Episode 1

--- Classic Puzzle-Medium/skynet_revolution_episode1.py
+++ Classic Puzzle-Medium/skynet_revolution_episode1.py
@@ -1,37 +1,61 @@
 import sys
 import math
 
-# Don't let the machines win. You are humanity's last hope...
-
-width = int(input())  # the number of cells on the X axis
-height = int(input())  # the number of cells on the Y axis
-matrix = []
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
 
 
-def find_closest(line, start=0):
-    for i, val in enumerate(line[start:]):
-        if val == '0':
-            return i + start
-    return -1
+class Node:
+    def __init__(self, i):
+        self.val = i
+        self._links = set()
+        self.exit = False
+
+    def connect(self, other):
+        self._links.add(other)
+        other._links.add(self)
+
+    def disconnect(self, other):
+        self._links.discard(other)
+        other._links.discard(self)
+
+    def __repr__(self):
+        return '<Node {}>'.format(self.val)
+
+    def pick_one_node(self):
+        # print(self._links, file=sys.stderr)
+        if not self._links:
+            return None
+        for node in self._links:
+            if node.exit:
+                self.disconnect(node)
+                return node
+        node = self._links.pop()
+        self.disconnect(node)
+        return node
+
+# n: the total number of nodes in the level, including the gateways
+# l: the number of links
+# e: the number of exit gateways
+n, l, e = [int(i) for i in input().split()]
+
+all_nodes = [Node(i) for i in range(n)]
+
+for i in range(l):
+    # n1: N1 and N2 defines a link between these nodes
+    n1, n2 = [int(j) for j in input().split()]
+    all_nodes[n1].connect(all_nodes[n2])
+    # print('{}-{}'.format(n1, n2), file=sys.stderr)
 
 
-for i in range(height):
-    line = input()  # width characters, each either 0 or .
-    matrix.append(line)
+for i in range(e):
+    ei = int(input())  # the index of a gateway node
+    all_nodes[ei].exit = True
 
-for y, line in enumerate(matrix):
-    x = find_closest(line)
-    while x >= 0:
-        output = [x, y]
-        right_x = find_closest(line, x + 1)
-        if right_x > x:
-            output.extend([right_x, y])
-        else:
-            output.extend([-1, -1])
-        bottom_y = find_closest([line[x] for line in matrix], y + 1)
-        if bottom_y > y:
-            output.extend([x, bottom_y])
-        else:
-            output.extend([-1, -1])
-        print(*output)
-        x = right_x
+# game loop
+while True:
+    si = int(input())  # The index of the node on which the Skynet agent is positioned this turn
+
+    node = all_nodes[si].pick_one_node()
+    if node is not None:
+        print(si, node.val)
